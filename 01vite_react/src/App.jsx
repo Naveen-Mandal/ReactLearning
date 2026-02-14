c8c512cc92fc6fa489838b1a8aaae63a2a6696ce
Naveen-Mandal <nmandal860@gmail.com>
2026-02-14 14:43:07 +0530

    custom react made, elements inside main made, direct element rendered

--- 01vite_react/src/App.jsx
+++ 01vite_react/src/App.jsx
@@ -1,8 +1,10 @@
 
-function App() {
+function App() { 
+  const anotherUser = "Hello this is another user"          //this is evaluated expression as children of that element or content of that element
   return (
     <>
      <h1>hello world with vite</h1>
+     <h1>how are you {anotherUser}</h1>    
     </>
   )
 }
--- 01vite_react/src/main.jsx
+++ 01vite_react/src/main.jsx
@@ -1,15 +1,32 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import React from 'react'
 
 import App from './App.jsx'
 import Hello from './Hello.jsx'
 
+
+function MyApp() {
+  return (
+    <h1>Hello this is MyApp finction inside main.jsx</h1>
+  )
+}
+
+const MyElement = (
+  <a href="https://google.com" target='_blank'>Vist google by clicking me</a>
+)
+
+const anotherUser = "Hello this is another user"
+const reactElement = React.createElement(
+  'a',  //this is element type
+  { href: 'https://google.com', target: '_blank' },   //this is attributes of that element
+  'Visit Google by clicking me',    //this is children of that element or content of that element
+  anotherUser    //this is evaluated expression as children of that element or content of that element
+)
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <>
-      <Hello/>
-      <App/>
-      <p>how are you</p>
-    </>
-  </StrictMode>,
+
+  // MyApp()
+  // <MyApp />
+  // MyElement
+  reactElement
 )
