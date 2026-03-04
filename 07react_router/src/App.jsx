4792957d0e2f34021dd4809c69babbb6e1c543a3
Naveen-Mandal <nmandal860@gmail.com>
2026-03-04 21:39:34 +0530

    react router initialized with tailwindcss

---
+++ 07react_router/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='text-3xl font-bold text-blue-500 bg-green-500 p-4'>Hello world!</div>
+    </>
+  )
+}
+
+export default App
